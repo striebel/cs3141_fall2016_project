be8eed2a2fd83d1576ba0ef4b2f41abe8e477f67
Jacob Striebel <striebel@users.noreply.github.com>
2016-09-28 20:40:19 -0400

    Add files for the initial extension template

---
+++ Sprint%201/menu_functions.js
@@ -0,0 +1,10 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	var button = document.getElementById('openMenu');
+	button.addEventListener('click', function() {
+		window.open("menu.html");
+	});
+});

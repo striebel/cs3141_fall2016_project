ae0f5717b4e71649d87d1480af34bbba543da30b
Collin Bernard <cbbernar@mtu.edu>
2016-09-29 21:05:26 -0400

    Changed names of sub menu pages

--- Sprint1/submenu_functions.js
+++ Sprint1/submenu_functions.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Created button that, when clicked, opens a new tab in chrome
 	var button = document.getElementById('item1'); 
 	button.addEventListener('click', function() {
-		window.open("menu_item1.html");
+		window.open("blacklist.html");
 	});
 });
 
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Created button that, when clicked, opens a new tab in chrome
 	var button = document.getElementById('item2'); 
 	button.addEventListener('click', function() {
-		window.open("menu_item2.html");
+		window.open("whitelist.html");
 	});
 });
 
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Created button that, when clicked, opens a new tab in chrome
 	var button = document.getElementById('item3'); 
 	button.addEventListener('click', function() {
-		window.open("menu_item3.html");
+		window.open("timer.html");
 	});
 });
 
@@ -30,6 +30,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Created button that, when clicked, opens a new tab in chrome
 	var button = document.getElementById('item4'); 
 	button.addEventListener('click', function() {
-		window.open("menu_item4.html");
+		window.open("daily_timer.html");
 	});
 });

477896e1ef9c3db352759bc4a6105905ec85cb48
Jake Mager <jmager@Jakes-MacBook-Pro.local>
2016-09-29 20:07:45 -0400

    Added options to the popup and a CSS file for the popup

--- Sprint1/menu_functions.js
+++ Sprint1/menu_functions.js
@@ -3,7 +3,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-	var button = document.getElementById('openMenu');
+	var button = document.getElementById('settings');
 	button.addEventListener('click', function() {
 		window.open("menu.html");
 	});

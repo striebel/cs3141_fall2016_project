dbfc8969737cd6963e26e1c10c0c6e454c941ed4
striebel <jacob.striebel@outlook.com>
2016-09-29 20:48:54 -0400

    Updated main menu to include submenus.

---
+++ Sprint1/submenu_functions.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Created button that, when clicked, opens a new tab in chrome
+	var button = document.getElementById('item1'); 
+	button.addEventListener('click', function() {
+		window.open("menu_item1.html");
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Created button that, when clicked, opens a new tab in chrome
+	var button = document.getElementById('item2'); 
+	button.addEventListener('click', function() {
+		window.open("menu_item2.html");
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Created button that, when clicked, opens a new tab in chrome
+	var button = document.getElementById('item3'); 
+	button.addEventListener('click', function() {
+		window.open("menu_item3.html");
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Created button that, when clicked, opens a new tab in chrome
+	var button = document.getElementById('item4'); 
+	button.addEventListener('click', function() {
+		window.open("menu_item4.html");
+	});
+});

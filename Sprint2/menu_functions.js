cf994ee04fe8ab297310dbb5804b2276c54df1ca
Collin Bernard <cbbernar@mtu.edu>
2016-10-10 12:10:04 -0400

    Copied and Pasted Sprint 1 files into Sprint 2

---
+++ Sprint2/menu_functions.js
@@ -0,0 +1,11 @@
+
+
+
+$( document ).ready(function() {
+
+	// Help button listener
+	var helpBtn = document.getElementById('help'); 
+	helpBtn.addEventListener('click', function() {
+		window.open("help.html");
+	});
+});
---
+++ Sprint2/submenu_functions.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Created button that, when clicked, opens a new tab in chrome
+	var button = document.getElementById('blacklistBtn'); 
+	button.addEventListener('click', function() {
+		window.open("blacklist.html");
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Created button that, when clicked, opens a new tab in chrome
+	var button = document.getElementById('whitelistBtn'); 
+	button.addEventListener('click', function() {
+		window.open("whitelist.html");
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Created button that, when clicked, opens a new tab in chrome
+	var button = document.getElementById('timerBtn'); 
+	button.addEventListener('click', function() {
+		window.open("timer.html");
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Created button that, when clicked, opens a new tab in chrome
+	var button = document.getElementById('dailyTimerBtn'); 
+	button.addEventListener('click', function() {
+		window.open("daily_timer.html");
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Created button that, when clicked, opens a new tab in chrome
+	var button = document.getElementById('todoListBtn'); 
+	button.addEventListener('click', function() {
+		window.open("todo.html");
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Created button that, when clicked, opens a new tab in chrome
+	var button = document.getElementById('helpBtn'); 
+	button.addEventListener('click', function() {
+		window.open("help.html");
+	});
+});

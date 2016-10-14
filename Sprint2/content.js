27ec196aff9943144ff3f18cc64bab0d2eb09fd2
jakemager <jmager12@gmail.com>
2016-10-14 11:34:05 -0400

    Created js content scripts to block websites. Still TODO switch my temp var to the actual imported blacklist from stroage

--- Sprint2/content.js
+++ Sprint2/content.js
@@ -1,1 +1,28 @@
-console.log("Current URL:" + window.location.toString());
+// Get current URL, then remove everything besides *.*
+var currentURL = window.location.toString().toLowerCase();
+currentURL = currentURL.replace("http://", "");
+currentURL = currentURL.replace("https://", "");
+currentURL = currentURL.replace("www.", "");
+var deleteIndex = currentURL.indexOf('/');
+currentURL = currentURL.substring(0, deleteIndex);
+
+// TODO: Import list from blacklist storage
+// Temporary List
+var tempBlacklist = "ask.com~yahoo.com~youtube.com";
+
+// Estiablish regex to match URLs
+var regex = new RegExp(currentURL);
+var blackList = tempBlacklist.split('~');
+for (var i = 0; i < blackList.length; i++) {
+	console.log(blackList[i])
+	if (currentURL.match(blackList[i])) {
+		blockWebsite();
+	}
+}
+
+// blockWebsite: Change inner HTML of HTML tags to tell the user they can not go to this site
+function blockWebsite() {
+		var d = new Date();
+		var n = d.getFullYear();
+		document.getElementsByTagName("html")[0].innerHTML = "<body><center><h1>STOP PROCRASTINATION " + n + "</h1><img src='http://www.mememaker.net/static/images/memes/4467057.jpg' /></center></body>";
+}
